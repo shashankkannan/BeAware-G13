cb483ce066dc9caa23ffd69f5ade575fd6f4bbcf
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2024-03-11 15:08:51 -0400

    Add beaware directory

---
+++ beaware/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SignUp  from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp /> 
+      {/*<SignIn />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ beaware/src/components/SignIn.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-in logic here
+    console.log('Sign In Form submitted:', formData);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
